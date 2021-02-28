060b2cd0e0dab171893c227300991fb2aab01aae
Ritik17 <ritik.singh@binarynumbers.io>
2021-02-28 16:43:11 +0530

    added keyboard inputs as well

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,10 +1,28 @@
 const calc = document.querySelector('.calc');
 const screen = document.querySelector('.screen');
 
-calc.addEventListener('click', function(e){
-    console.log(e.target.tagName);
-    if(e.target.tagName === 'BUTTON'){
-    screen.innerHTML = '';
-    screen.innerHTML = e.target.innerHTML;
+function specialFunc(e){
+    let input;
+    console.log(e);
+    console.log(e.type);
+    if(e.type === 'click' && e.target.tagName === 'BUTTON'){
+        input = e.target.innerText;
     }
-})
+    else if(e.type === 'keyup'){
+        input = e.key;
+    }
+    console.log(input);
+    if(!isNaN(parseInt(input)) && screen.innerText.length < 15){
+    screen.innerHTML = screen.innerHTML + input;
+    }
+    else if(input.toUpperCase() === 'C'){
+        screen.innerText = '';
+    }
+    else if(input === '←' || input.toUpperCase() === 'BACKSPACE'){
+        console.log(screen.innerText);
+        screen.innerText = screen.innerText.slice(0, screen.innerText.length-1);
+    }
+}
+
+window.addEventListener('click', specialFunc);
+window.addEventListener('keyup', specialFunc);
