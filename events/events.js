e5579e9f40fd1cf76988ebc248a61fdbe8b7bb33
Ritik17 <ritik.singh@binarynumbers.io>
2021-03-18 21:44:52 +0530

    improved the ui, added remove functionality

--- events/events.js
+++ events/events.js
@@ -1,54 +1,62 @@
-newEvent=document.querySelector('#newEvent');
-form=document.querySelector('form');
+newEvent = document.querySelector("#newEvent");
+section = document.querySelector("section");
 
-newEvent.onclick=()=>{
-    form.style.display='flex';
+newEvent.onclick = () => {
+  section.style.display = "flex";
+};
+section.addEventListener("click", function (e) {
+  if (e.target.nodeName === "SECTION") {
+    section.style.display = "none";
+  }
+});
+
+section.onsubmit = () => {
+  section.style.display = "none";
+  EventCard();
+  return false;
+};
+function EventCard() {
+  let inputs = document.querySelectorAll("input");
+
+  let eventF = inputs[0].value;
+  let descF = inputs[1].value;
+  let dinankF = inputs[2].value;
+  let samayF = inputs[3].value;
+
+  const eventName = document.createElement("span");
+  const Description = document.createElement("span");
+  const Dinank = document.createElement("span");
+  const Samay = document.createElement("span");
+  const removeButton = document.createElement("span");
+
+  removeButton.innerText = "remove";
+
+  div = document.createElement("div");
+  div.setAttribute("id", "event");
+
+  eventName.setAttribute("id", "eventName");
+  Description.setAttribute("id", "desc");
+  Dinank.setAttribute("id", "date");
+  Samay.setAttribute("id", "time");
+  removeButton.setAttribute("id", "rmvBtn");
+
+  eventName.textContent = eventF;
+  Description.textContent = descF;
+  Dinank.textContent = dinankF;
+  Samay.textContent = samayF;
+
+  div.appendChild(eventName);
+  div.appendChild(Description);
+  div.appendChild(Dinank);
+  div.appendChild(Samay);
+  div.appendChild(removeButton);
+
+  document.querySelector("main").appendChild(div);
 }
-form.onsubmit=()=>{
-    form.style.display='none';
-    EventCard();
-    return false;
-}
-function EventCard(){
-    let inputs=document.querySelectorAll('input');
 
-    let eventF=inputs[0].value;
-    let descF=inputs[1].value;
-    let dinankF=inputs[2].value;
-    let samayF=inputs[3].value;
-
-    Event= document.createElement('span');
-    Description= document.createElement('span');
-    Dinank= document.createElement('span');
-    Samay= document.createElement('span');
-    removeButton= document.createElement('span');
-
-    removeButton.innerText="remove";
-
-    div=document.createElement('div');
-
-    Event.setAttribute('id', 'event');
-    Description.setAttribute('id', 'desc');
-    Dinank.setAttribute('id', 'date');
-    Samay.setAttribute('id', 'time');
-    removeButton.setAttribute('id', 'rmvBtn')
-
-    Event.textContent=eventF;
-    Description.textContent=descF;
-    Dinank.textContent=dinankF;
-    Samay.textContent=samayF;
-
-    div.appendChild(Event);
-    div.appendChild(Description);
-    div.appendChild(Dinank);
-    div.appendChild(Samay);
-    div.appendChild(removeButton);
-
-    console.log(div);
-    document.querySelector('main').appendChild(div);
-
-    document.querySelector("#rmvBtn").addEventListener('click', function remove(){
-
-})
-}
-
+window.addEventListener("click", function (e) {
+  if (e.target.id === "rmvBtn") {
+    const eventEl = e.target.parentElement;
+    eventEl.remove();
+  }
+});
