c87f99c5828e90b5339b773373640ef083ad15d6
Ritik17 <ritik.singh@binarynumbers.io>
2021-03-20 15:36:33 +0530

    added local storage functionality

--- events/events.js
+++ events/events.js
@@ -1,32 +1,31 @@
 let newEvent = document.querySelector("#newEvent");
 let section = document.querySelector("section");
 let close = document.querySelector("h4");
+let notes = [];
 
-newEvent.onclick = () => {
-  section.style.display = "flex";
-};
-section.addEventListener("click", function (e) {
-  if (e.target.nodeName === "SECTION") {
-    section.style.display = "none";
-  } else if (e.target.nodeName === "H4") {
-    section.style.display = "none";
+// retrieves data from local storage and renders it to DOM, if it exists
+if (supportsLocalStorage() && window.localStorage.notesArray) {
+  notes = JSON.parse(window.localStorage.notesArray);
+  if (notes !== "[]") {
+    window.addEventListener("DOMContentLoaded", function () {
+      notes.map(function (note) {
+        createEventCard(note.name, note.desc, note.dinankF, note.samayF);
+      });
+    });
   }
-});
+}
 
-section.onsubmit = () => {
-  section.style.display = "none";
-  EventCard();
-  return false;
-};
-function EventCard() {
-  let inputs = document.querySelectorAll("input");
-  let textarea = document.querySelector("textarea");
+// checks whether browser supports local storage or not
+function supportsLocalStorage() {
+  if (typeof Storage !== undefined) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-  let eventF = inputs[0].value;
-  let descF = textarea.value;
-  let dinankF = inputs[1].value;
-  let samayF = inputs[2].value;
-
+// sets up a div for containing the event/note details
+function createEventCard(valueOne, valueTwo, valueThree, valueFour) {
   const eventName = document.createElement("span");
   const Description = document.createElement("span");
   const Dinank = document.createElement("span");
@@ -44,10 +43,10 @@ function EventCard() {
   Samay.setAttribute("id", "time");
   removeButton.setAttribute("id", "rmvBtn");
 
-  eventName.textContent = eventF;
-  Description.textContent = descF;
-  Dinank.textContent = dinankF;
-  Samay.textContent = samayF;
+  eventName.textContent = valueOne;
+  Description.textContent = valueTwo;
+  Dinank.textContent = valueThree;
+  Samay.textContent = valueFour;
 
   div.appendChild(eventName);
   div.appendChild(Description);
@@ -58,9 +57,67 @@ function EventCard() {
   document.querySelector("main").appendChild(div);
 }
 
-window.addEventListener("click", function (e) {
+// add the new event/note to DOM as well as from local storage
+function AddEventCard() {
+  let inputs = document.querySelectorAll("input");
+  let textarea = document.querySelector("textarea");
+
+  let eventF = inputs[0].value;
+  let descF = textarea.value;
+  let dinankF = inputs[1].value;
+  let samayF = inputs[2].value;
+
+  createEventCard(eventF, descF, dinankF, samayF);
+
+  if (supportsLocalStorage()) {
+    let notesObj = {
+      name: eventF,
+      desc: descF,
+      date: dinankF,
+      time: samayF,
+    };
+    notes.push(notesObj);
+    window.localStorage.notesArray = JSON.stringify(notes);
+  }
+}
+
+// removes the target event/note from DOM as well as from local storage
+function RemoveEventCard(e) {
   if (e.target.id === "rmvBtn") {
     const eventEl = e.target.parentElement;
     eventEl.remove();
+    if (supportsLocalStorage()) {
+      notes.forEach(function (note, index) {
+        if (
+          note.title === e.target.parentElement.children[0].children.textContent
+        ) {
+          notes.splice(index, 1);
+          window.localStorage.notesArray = JSON.stringify(notes);
+        }
+      });
+    }
+  }
+}
+
+// showing the popup
+newEvent.onclick = () => {
+  section.style.display = "flex";
+};
+
+// hiding the popup
+section.addEventListener("click", function (e) {
+  if (e.target.nodeName === "SECTION") {
+    section.style.display = "none";
+  } else if (e.target.nodeName === "H4") {
+    section.style.display = "none";
   }
 });
+
+// submits the form, thereby rendering the card to dom and hiding the popup
+section.onsubmit = () => {
+  section.style.display = "none";
+  AddEventCard();
+  return false;
+};
+
+window.addEventListener("click", RemoveEventCard);
